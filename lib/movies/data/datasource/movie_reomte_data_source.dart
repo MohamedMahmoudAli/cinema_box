17502cf9487e78f1e69df7f90e0888feaff7466e
MohamedMahmoudAli <mohamed238ma@gmail.com>
2026-01-22 20:35:03 +0200

    finish the remote data source

--- lib/movies/data/datasource/movie_reomte_data_source.dart
+++ lib/movies/data/datasource/movie_reomte_data_source.dart
@@ -1,30 +1,61 @@
+import 'package:cinema_box/core/utils/constants/constants.dart';
 import 'package:cinema_box/movies/data/model/movies_model.dart';
-import 'package:cinema_box/movies/domain/entities/Movie.dart';
+import 'package:dio/dio.dart';
 
 abstract class BaseMovieRemoteDataSource {
   Future<List<MoviesModel>> getNowPlayingMovies();
-  Future <List<MoviesModel>> getPopularMovies();
-  Future <List<MoviesModel>> getTopRatedMovies();
-
+  Future<List<MoviesModel>> getPopularMovies();
+  Future<List<MoviesModel>> getTopRatedMovies();
 }
-class MovieRemoteDataSource extends BaseMovieRemoteDataSource{
+
+class MovieRemoteDataSource extends BaseMovieRemoteDataSource {
   @override
-  Future<List<MoviesModel>> getNowPlayingMovies() {
-    // TODO: implement getNowPlayingMovies
-    throw UnimplementedError();
+  Future<List<MoviesModel>> getNowPlayingMovies() async {
+    try {
+      final response = await Dio().get(
+        '${Constants.baseUrl}movie/now_playing?api_key=${Constants.apiKey}',
+      );
+      return List<MoviesModel>.from(
+        response.data['results'].map((e) => MoviesModel.fromJson(e)),
+      );
+    } catch (e) {
+      throw e.toString();
+    }
   }
 
   @override
-  Future<List<MoviesModel>> getPopularMovies() {
-    // TODO: implement getPopularMovies
-    throw UnimplementedError();
+  Future<List<MoviesModel>> getPopularMovies() async {
+    try {
+      final response = await Dio().get(
+        '${Constants.baseUrl}movie/popular?api_key=${Constants.apiKey}',
+      );
+      if (response.statusCode == 200) {
+        return List<MoviesModel>.from(
+          response.data['results'].map((e) => MoviesModel.fromJson(e)),
+        );
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } catch (e) {
+      throw e.toString();
+    }
   }
 
   @override
-  Future<List<MoviesModel>> getTopRatedMovies() {
-    // TODO: implement getTopRatedMovies
-    throw UnimplementedError();
+  Future<List<MoviesModel>> getTopRatedMovies() async {
+    try {
+      final Response = await Dio().get(
+        '${Constants.baseUrl}movie/top_rated?api_key=${Constants.apiKey}',
+      );
+      if (Response.statusCode == 200) {
+        return List<MoviesModel>.from(
+          Response.data['results'].map((e) => MoviesModel.fromJson(e)),
+        );
+      } else {
+        throw Exception(Response.statusMessage);
+      }
+    } catch (e) {
+      throw e.toString();
+    }
   }
- 
-  
-  }
+}
